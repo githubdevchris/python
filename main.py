9119eb9da0bac21209de610da684c9f35a954f33
Christian Burger <christian.b@bluepoint-services.at>
2021-03-08 08:10:38 +0100

    changes

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-print('Hier ist eine Nummer: %s Hier ein String: %s' %(1231.100, 'Hie'))
-
-list=[1,2,3,4,5,6,7,8,9]
-print('Hier ist der Eintrag in der Liste: ',list)
-print(list[:3])
+t =(1,2,3)
+print(t[0])
